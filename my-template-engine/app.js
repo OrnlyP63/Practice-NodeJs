4b36c3eee5d64aa1e1ba5431a0983fed718ca66b
OrnlyP63 <phiphat_c@cmu.ac.th>
2021-10-05 09:12:12 +0700

    JSOM

--- my-template-engine/app.js
+++ my-template-engine/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'all-views');
 app.get('/', function(req, res){
+  fs.readFile('./data.json', (err, data)=>{
+    const listObj = JSON.parse(data);
+    if (err) {
+      res.status(400).send('Error List not found');
+    } else {
+      res.render('first_template', {lists: listObj});
+    }
+  })
   //res.status(200).send('Hello World.');
-  res.render('first_template', {name: 'Phiphat'})
+  //res.render('first_template', {name: 'Phiphat'})
 });
 
 app.listen(3000, function(){
